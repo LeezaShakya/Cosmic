75815ab4f866b1c4dc7fb71d5458f0bdde2b11eb
Leeza@218 <leezashakya218@gmail.com>
2024-01-10 17:04:00 +0545

    second-commit

--- index.php
+++ index.php
@@ -1,39 +1,45 @@
 <?php include('include/header.php') ?>
-<section class="uk-section uk-main-slider">
+<section class="uk-section ">
     <div class=" uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="animation: fade;  autoplay: false; autoplay-interval: 3000;">
         <ul class="uk-slideshow-items  uk-padding-left-large" style=" height:100vh;" uk-height-viewport="offset-top: true; offset-bottom: 0.1">
             <li>
                 <div class="uk-child-width-1-2@m uk-grid-small " uk-grid>
-                    <div class=" uk-main-slider" style="padding:100px;">
-                        <h1 uk-slideshow-parallax="x: 100,-100">We Are Cosmic Insurance Brokerage</h1>
+                    <div class=" uk-main-slider" >
+                        <h5 uk-slideshow-parallax="x: 100,-100">We Are Cosmic Insurance Brokerage</h5>
                         <p class="uk-margin-large-bottom uk-banner-paragraph" uk-slideshow-parallax="x: 100,-100">Protecting dreams, securing futures – your trusted partner in every step, always there when needed</p>
-                        <a href="#" class="uk-blue-button" >LEARN MORE</a>
+                        <div>
+                            <a href="#" class="uk-blue-button" >LEARN MORE</a>
+                        </div>
                     </div>
-                    <div class="uk-main-slider-img uk-padding-remove">
+                    <div class="uk-main-slider-img uk-padding-remove uk-visible@l">
                         <img src="assets/img/banner.png" alt="">
                     </div>
                 </div>
             </li>
             <li>
                 <div class="uk-child-width-1-2@m uk-grid-small " uk-grid>
-                    <div class=" uk-main-slider" style="padding:100px;">
-                        <h1 uk-slideshow-parallax="x: 100,-100">We Are Cosmic Insurance Brokerage</h1>
+                    <div class=" uk-main-slider" >
+                        <h5 uk-slideshow-parallax="x: 100,-100">We Are Cosmic Insurance Brokerage</h5>
                         <p class="uk-margin-large-bottom uk-banner-paragraph" uk-slideshow-parallax="x: 100,-100">Protecting dreams, securing futures – your trusted partner in every step, always there when needed</p>
-                        <a href="#" class="uk-blue-button" >LEARN MORE</a>
+                        <div>
+                            <a href="#" class="uk-blue-button" >LEARN MORE</a>
+                        </div>
                     </div>
-                    <div class="uk-main-slider-img uk-padding-remove">
+                    <div class="uk-main-slider-img uk-padding-remove uk-visible@l">
                         <img src="assets/img/about.png" alt="">
                     </div>
                 </div>
             </li>
             <li>
                 <div class="uk-child-width-1-2@m uk-grid-small " uk-grid>
-                    <div class=" uk-main-slider" style="padding:100px;">
-                        <h1 uk-slideshow-parallax="x: 100,-100">We Are Cosmic Insurance Brokerage</h1>
-                        <p class="uk-margin-large-bottom " uk-slideshow-parallax="x: 100,-100">Protecting dreams, securing futures – your trusted partner in every step, always there when needed</p>
-                        <a href="#" class="uk-blue-button" >LEARN MORE</a>
+                    <div class=" uk-main-slider">
+                        <h5 uk-slideshow-parallax="x: 100,-100">We Are Cosmic Insurance Brokerage</h5>
+                        <p class="uk-margin-large-bottom uk-banner-paragraph" uk-slideshow-parallax="x: 100,-100">Protecting dreams, securing futures – your trusted partner in every step, always there when needed</p>
+                        <div>
+                            <a href="#" class="uk-blue-button" >LEARN MORE</a>
+                        </div>
                     </div>
-                    <div class="uk-main-slider-img uk-padding-remove">
+                    <div class="uk-main-slider-img uk-padding-remove uk-visible@l">
                         <img src="assets/img/banner.png" alt="">
                     </div>
                 </div>
@@ -43,14 +49,136 @@
         <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
     </div>
 </section>
-<section class="uk-section section-padding">
-    <div class="uk-container" >
-       <div>
+<!-- SERVICES SECTION -->
+<section class="uk-section section-padding grey-bg">
+    <div class="uk-container uk-text-center">
        <h1>SERVICES</h1>
-        <h2>Cosmic Insurance Brokerage Insurance Solutions for You</h2>
-       </div>
-       <h3>section-heading</h3>
-       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto vero laboriosam nesciunt maxime incidunt. Ab inventore iusto ipsa eveniet quis delectus consequuntur a, aliquid, iure aperiam magni voluptates vel incidunt!</p>
-</div>
+       <h2>What We Are Offering</h2>
+        <div class="uk-slider-container-offset" uk-slider>
+            <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1">
+                <ul class="uk-slider-items uk-child-width-1-3@s  uk-grid uk-margin-large-top " uk-height-match=".section-card">
+                    <li>
+                        <div class="uk-card uk-card-default section-card">
+                            <div class="uk-card-media-top uk-padding-small uk-padding-remove-bottom">
+                                <img src="assets/img/wallet-travel.png" width="80" height="1200" alt="">
+                            </div>
+                            <div class="uk-card-body uk-padding-small">
+                                <h3 >Travels Insurance</h3>
+                                <p>Insurance policies may also include other benefits such as coverage for external factors.</p>
+                                <a href="#" class="servives-button">Learn More<span uk-icon="icon: chevron-double-right"></span></a>
+                            </div>
+                        </div>
+                    </li>
+                    <li >
+                        <div class="uk-card uk-card-default section-card">
+                            <div class="uk-card-media-top uk-padding-small uk-padding-remove-bottom">
+                                <img src="assets/img/train-car.png" width="80" height="1200" alt="">
+                            </div>
+                            <div class="uk-card-body uk-padding-small">
+                                <h3 >Vehicle Insurance</h3>
+                                <p>Insurance policies may also include other benefits such as coverage for external factors.</p>
+                                <a href="#" class="servives-button">Learn More<span uk-icon="icon: chevron-double-right"></span></a>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="uk-card uk-card-default section-card">
+                            <div class="uk-card-media-top uk-padding-small uk-padding-remove-bottom">
+                                <img src="assets/img/hand-heart.png" width="80" height="1200" alt="">
+                            </div>
+                            <div class="uk-card-body uk-padding-small">
+                                <h3 >Marriage Insurance</h3>
+                                <p>Insurance policies may also include other benefits such as coverage for external factors.</p>
+                                <a href="#" class="servives-button">Learn More<span uk-icon="icon: chevron-double-right"></span></a>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="uk-card uk-card-default section-card">
+                            <div class="uk-card-media-top uk-padding-small uk-padding-remove-bottom">
+                                <img src="assets/img/wallet-travel.png" width="80" height="1200" alt="">
+                            </div>
+                            <div class="uk-card-body uk-padding-small">
+                                <h3 >Travels Insurance</h3>
+                                <p>Insurance policies may also include other benefits such as coverage for external factors.</p>
+                                <a href="#" class="servives-button">Learn More<span uk-icon="icon: chevron-double-right"></span></a>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                <a class="uk-position-center-left uk-position-small uk-hidden-hover" href uk-slidenav-previous uk-slider-item="previous"></a>
+                <a class="uk-position-center-right uk-position-small uk-hidden-hover" href uk-slidenav-next uk-slider-item="next"></a>
+            </div>
+            <ul class="uk-slider-nav uk-dotnav uk-flex-center  uk-margin-large-top"></ul>
+        </div>
+    </div>
 </section>
+<!-- END OF SERVICES -->
+<!-- news and events section -->
+<section class="uk-section section-padding">
+    <div class="uk-container uk-text-center">
+        <h1>NEWS & EVENTS</h1>
+        <h2>Know More Updates About Us</h2>
+        <ul class=" uk-child-width-expand@m uk-margin-large-top" uk-grid uk-height-match=".news-block"  >
+            <li class="">
+                <div class="news-block uk-text-left ">
+                <img src="assets/img/news1.png" alt="">
+                <div class="news-content">
+                    <p class=" uk-margin-small"> <span uk-icon="icon: calendar" style="color: var(--secondary);"></span> Kathmandu, October 12th, 2023 </p>
+                    <h3><a href="#">Insurance policies may also include other policies</a></h3>
+                </div>
+                </div>
+            </li>
+            <li class="uk-text-center">
+                <div class="news-block uk-text-left ">
+                <img src="assets/img/news2.png" alt="">
+                <div class="news-content">
+                    <p class=" uk-margin-small"> <span uk-icon="icon: calendar" style="color: var(--secondary);"></span> Kathmandu, October 12th, 2023 </p>
+                    <h3><a href="#">Insurance policies may also include other policies</a></h3>
+                </div>
+                </div>
+            </li>
+            <li class="uk-text-center  ">
+                <div class="news-block uk-text-left ">
+                <img src="assets/img/news3.png" alt="">
+                <div class="news-content">
+                    <p class=" uk-margin-small"> <span uk-icon="icon: calendar" style="color: var(--secondary);"></span> Kathmandu, October 12th, 2023 </p>
+                    <h3><a href="#">Insurance policies may also include other policies</a></h3>
+                </div>
+                </div>
+            </li>
+        </ul>
+    </div> 
+    <div class="uk-text-center">
+        <a href="" class="text-light uk-button uk-primary uk-margin-top">LEARN MORE</a>
+    </div>
+</section>
+<!--end of  news and events section -->
+<!-- ad section -->
+<section class="uk-section section-padding uk-container">
+    <div class="uk-container black-bg">
+    <ul class=" uk-child-width-1-2@s  uk-padding  " uk-grid >
+            <li class="uk-content-center">
+                <div class="">
+                    <h2 style="color:white;">Save money on insurance with Cosmic Insurance Brokerage</h2>
+                </div>
+                <div class="uk-flex ">
+                    <div class="uk-margin-right"> <a href="" class="text-light uk-button uk-primary uk-margin-top">LEARN MORE</a></div>
+                    <div class="uk-margin-top ">
+                        <p style="color:white; margin:0; font-size:20px !important; font-weight:500;">CALL US</p>
+                        <p style="color:white; margin:0; font-size:18px !important; font-weight:500;">+123 456 789</p>
+                    </div>
+                </div>
+            </li>
+            <li class="uk-text-center  ">
+                <div class="">
+                    <img src="assets/img/pngegg.png" alt=""  style="height:300px;">
+                </div>
+            </li>
+        </ul>
+    </div>
+    </div>
+</section>
+<!-- end of add section -->
+
 <?php include('include/footer.php') ?> 
